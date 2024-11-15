b4d50fa2453d6706e966f1981f2624413a53d4ca
anthony <b09902033@csie.ntu.edu.tw>
2024-11-16 01:27:20 +0800

    support folder as input

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ from module.textaligner import TextAligner
 from argparse import ArgumentParser
 import time
 from copy import deepcopy
+import os.path as osp
+import os
+from pydub import AudioSegment
 
 
 TOKENIZER_REPO = "voidful/Llama-3.2-11B-ASR"
@@ -29,6 +32,7 @@ PAD_TOKEN_ID = 51867
 XVECTOR_SR = 16000
 MIMI_SR = 24000
 BATCH_SIZE = 16
+BATCH_SIZE_SEC = 400
     
 
 mimi = MimiModel()
@@ -54,6 +58,7 @@ def dump_sample(batch_frame_list, audio_file, speaker_id, sample_path):
         audio = audio_data_manager.get(audio_file, speaker_id, XVECTOR_SR)
         audio = audio[int(start_sec*XVECTOR_SR):int(end_sec*XVECTOR_SR)]
         batch_audio.append(audio)
+    # print([a.shape for a in batch_audio])
     # sf.write(sample_path, audio, 16000) # data should be 1-dim tensor
     batch_spk_emb = xvector.encode(batch_audio, XVECTOR_SR)
 
@@ -74,6 +79,8 @@ def dump_sample(batch_frame_list, audio_file, speaker_id, sample_path):
         frame_list = batch_frame_list[i]
         codec_length = codes.shape[-1]
         raw_text, text_with_pad = text_aligner.pad(frame_list, codec_length)
+        if raw_text is None or text_with_pad is None:
+            return {k: [] for k in ["text", "text_with_pad", "unit", "x-vector"]}
         batch_raw_text.append(raw_text)
         batch_text_with_pad.append(text_with_pad)
     
@@ -87,90 +94,116 @@ def dump_sample(batch_frame_list, audio_file, speaker_id, sample_path):
     return sample_obj
 
 
-def sort_duration(clips):
-    return sorted(clips, key=lambda x: x.end_sec - x.start_sec)
+def convert_to_wav(audio_file):
+    """ Convert `audio_file` from m4a to wav, and return the path to the wav file. """
+    # Ensure the file has a valid extension
+    if not audio_file.lower().endswith(".m4a"):
+        raise ValueError("Input file must be in .m4a format")
+    
+    wav_file = osp.splitext(audio_file)[0] + ".wav"
+    
+    audio = AudioSegment.from_file(audio_file, format="m4a")
+    audio.export(wav_file, format="wav")
+    
+    return wav_file
 
 
-def main():
-    trans_file = "/work/u3937558/Soundon-TTS/dev/tw_transcription/word/8199eec0-9a0f-4561-9053-ee1bddddccd1/25fa8eda-af76-4637-8820-f10fe5166e31.csv"
-    # audio_file = "/work/u3937558/Soundon-TTS/dev/tw_separated/8199eec0-9a0f-4561-9053-ee1bddddccd1/25fa8eda-af76-4637-8820-f10fe5166e31.m4a"
-    audio_file = "../MMLM/source.wav"
-
-    speaker_set = set()
-    with open(trans_file) as csvfile:
-        start_time = time.time()
-        reader = csv.reader(csvfile)
-        next(reader, None)
-
-        # Step 1: read all word frames
-        frame_list = []
-        for row in reader:
-            frame = WordFrame(
-                speaker = int(row[0]),
-                start_sec = float(row[1][:-1]),
-                end_sec = float(row[2][:-1]),
-                content = row[3]
-            )
-            speaker_set.add(int(row[0]))
-            frame_list.append(frame)
-
-
-        # Step 2: concatenate frames into samples. Split at silence >= 0.3 second.
-        clips = {spk: [] for spk in speaker_set}
-        for frame in frame_list:
-            spk_clips = clips[frame.speaker]
-            # Only run at the first frame of every speaker
-            if not spk_clips:
-                spk_clips.append([frame])
-            if frame.start_sec - spk_clips[-1][-1].end_sec >= 0.3:
-                spk_clips.append([frame])
-            else:
-                spk_clips[-1].append(frame)
-
-        # for spk in speaker_set:
-        #     for clip in clips[spk]:
-        #         print(clip[0].start_sec, clip[-1].end_sec, clip[-1].end_sec - clip[0].start_sec)
-        end_time = time.time()
-        print("[debug] Step 1, 2", end_time - start_time)
-
-
-        # Step 3: extract unit and x-vector
+def main(root_folder):
+    for channel_id in os.listdir(osp.join(root_folder, "tw_separated")):
         dataset_dict = {"unit": [], "x-vector": [], "text": [], "text_with_pad": []}
-        for spk in speaker_set:
-            # patient = 10
-            idx = 0
-            spk_dataset_dict = {"unit": [], "x-vector": [], "text": [], "text_with_pad": []}
-            batch_clip = []
-            clips[spk].sort(key=lambda clip: clip[-1].end_sec - clip[0].start_sec)
-            for clip in tqdm(clips[spk]):
-                if clip[-1].end_sec - clip[0].start_sec < 0.3:
-                    continue
-                batch_clip.append(clip)
+        for audio_file in os.listdir(osp.join(root_folder, "tw_separated", channel_id)):
+            # trans_file = "/work/u3937558/Soundon-TTS/dev/tw_transcription/word/8199eec0-9a0f-4561-9053-ee1bddddccd1/25fa8eda-af76-4637-8820-f10fe5166e31.csv"
+            # audio_file = "/work/u3937558/Soundon-TTS/dev/tw_separated/8199eec0-9a0f-4561-9053-ee1bddddccd1/25fa8eda-af76-4637-8820-f10fe5166e31.m4a"
+            # audio_file = "../MMLM/source.wav"
+            if not audio_file.endswith(".m4a"):
+                continue
+            audio_id = audio_file[:-4]
+            trans_file = osp.join(root_folder, "tw_transcription/word", channel_id, audio_id + ".csv")
+            audio_file = osp.join(root_folder, "tw_separated", channel_id, audio_id + ".m4a")
 
-                if len(batch_clip) == BATCH_SIZE:
-                    sample_obj = dump_sample(batch_clip, audio_file, speaker_id=spk, sample_path=f"./output-{idx}.wav")
-                # idx += 1
-                    spk_dataset_dict["text"].extend(sample_obj["text"]) 
-                    spk_dataset_dict["text_with_pad"].extend(sample_obj["text_with_pad"]) 
-                    spk_dataset_dict["unit"].extend(sample_obj["unit"]) 
-                    spk_dataset_dict["x-vector"].extend(sample_obj["x-vector"]) 
+            audio_file = convert_to_wav(audio_file)
+
+            speaker_set = set()
+            with open(trans_file) as csvfile:
+                start_time = time.time()
+                reader = csv.reader(csvfile)
+                next(reader, None)
+
+                # Step 1: read all word frames
+                frame_list = []
+                for row in reader:
+                    frame = WordFrame(
+                        speaker = int(row[0]),
+                        start_sec = float(row[1][:-1]),
+                        end_sec = float(row[2][:-1]),
+                        content = row[3]
+                    )
+                    speaker_set.add(int(row[0]))
+                    frame_list.append(frame)
+
+
+                # Step 2: concatenate frames into samples. Split at silence >= 0.3 second.
+                clips = {spk: [] for spk in speaker_set}
+                for frame in frame_list:
+                    spk_clips = clips[frame.speaker]
+                    # Only run at the first frame of every speaker
+                    if not spk_clips:
+                        spk_clips.append([frame])
+                    if frame.start_sec - spk_clips[-1][-1].end_sec >= 0.3:
+                        spk_clips.append([frame])
+                    else:
+                        spk_clips[-1].append(frame)
+
+                # for spk in speaker_set:
+                #     for clip in clips[spk]:
+                #         print(clip[0].start_sec, clip[-1].end_sec, clip[-1].end_sec - clip[0].start_sec)
+                # end_time = time.time()
+                # print("[debug] Step 1, 2", end_time - start_time)
+
+
+                # Step 3: extract unit and x-vector
+                for spk in speaker_set:
+                    # patient = 10
+                    idx = 0
+                    spk_dataset_dict = {"unit": [], "x-vector": [], "text": [], "text_with_pad": []}
                     batch_clip = []
+                    clips[spk].sort(key=lambda clip: clip[-1].end_sec - clip[0].start_sec)
+                    for clip in tqdm(clips[spk]):
+                        if clip[-1].end_sec - clip[0].start_sec < 0.3:
+                            continue
+                        batch_clip.append(clip)
 
-                # patient -= 1
-                # if patient < 0:
-                #     break
-            random.shuffle(spk_dataset_dict["x-vector"])
-            for colname in ["unit", "x-vector", "text", "text_with_pad"]:
-                dataset_dict[colname].extend(spk_dataset_dict[colname])
+                        max_audio_sec = clip[-1].end_sec - clip[0].start_sec
+                        if len(batch_clip) * max_audio_sec >= BATCH_SIZE_SEC:
+                            sample_obj = dump_sample(batch_clip, audio_file, speaker_id=spk, sample_path=f"./output-{idx}.wav")
+                        # idx += 1
+                            spk_dataset_dict["text"].extend(sample_obj["text"]) 
+                            spk_dataset_dict["text_with_pad"].extend(sample_obj["text_with_pad"]) 
+                            spk_dataset_dict["unit"].extend(sample_obj["unit"]) 
+                            spk_dataset_dict["x-vector"].extend(sample_obj["x-vector"]) 
+                            batch_clip = []
+                            torch.cuda.empty_cache()
 
+
+                        # patient -= 1
+                        # if patient < 0:
+                        #     break
+                    random.shuffle(spk_dataset_dict["x-vector"])
+                    for colname in ["unit", "x-vector", "text", "text_with_pad"]:
+                        dataset_dict[colname].extend(spk_dataset_dict[colname])
+
+            os.remove(audio_file)
+            audio_data_manager.pop(audio_file)
+            torch.cuda.empty_cache()
         ds = Dataset.from_dict(dataset_dict) 
-        ds.save_to_disk("moshi_tts_dataset_dummy")
+        ds.save_to_disk(osp.join("moshi_tts_dataset", channel_id))
         # ds.push_to_hub("anthony-wss/moshi_tts_dataset_dummy")    
         print(ds)
-            # print(', '.join(row))
+        # print(', '.join(row))
 
 if __name__ == "__main__":
-    # parser = ArgumentParser()
-    # args = parser.parse_args()
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("--root_folder", required=True, help="path to folder with tw_separated and tw_transcription")
+    args = parser.parse_args()
+    main(args.root_folder)
 
--- module/textaligner.py
+++ module/textaligner.py
@@ -8,6 +8,8 @@ class TextAligner:
     def pad(self, frame_list, sequence_length):
         """
         Generate moshi-style text_with_pad with length equals to sequence_length(codec length)
+
+        Warning: the return value can be both None for weird data
         """
         start_sec = frame_list[0].start_sec
         raw_text = ""
@@ -15,33 +17,57 @@ class TextAligner:
         for frame in frame_list:
             timestamp = frame.start_sec - start_sec
             insert_idx = int((timestamp*1000)//80)
+
+            # Edge case: timestamp can be very close to clip end, simpley discard that text
+            if insert_idx >= sequence_length:
+                break
+
             # Seperate two close tokens
+            flag_to_break = False
             while text[insert_idx] != self.PAD_TOKEN:
                 if insert_idx + 1 >= sequence_length:
-                    continue  # Skip this text
+                    flag_to_break = True
+                    break  # Skip this text
                 else:
                     insert_idx += 1
+            if flag_to_break:
+                break
 
             text_token = self.tokenizer.encode(frame.content, add_special_tokens=False)
             
-            text[insert_idx] = frame.content
-            raw_text += frame.content
+            # If the last text is encoded into multiple token that exceed sequence length, discard that text
+            if insert_idx + len(text_token) - 1 >= sequence_length:
+                break
+            else:
+                text[insert_idx] = frame.content
+                raw_text += frame.content
 
-            # The encoded text can be longer than one token
-            for i in range(1, len(text_token)):
-                text[insert_idx+i] = "DEL"
+                # The encoded text can be longer than one token
+                for i in range(1, len(text_token)):
+                    text[insert_idx+i] = "DEL"
 
         # _text = deepcopy(text)  # For debug
         text = [t for t in text if t != "DEL"]
 
-        # Special case: 兩個連太近的字、中間沒 pad。
+        # Special case 1: 兩個連太近的字、中間沒 pad。
         # Example: '以', '前', '以前'都是一個 token，有時 whisper 不會 predict '以前'，而是分成'以'、'前'兩個 frame
         def handler(text_list):
-            # 統一 pad 在後面
+            if len(text_list) == 1:
+                return text_list
+            # Special case 1-1: 統一 pad 在後面
             # Example: handler(['以', '前', '我', '在']) -> ['以', '前', '我', '在', PAD_TOKEN]
             # 因為「以前我在」會被 tokenize 成 3 個 tokens
             actual_tok_len = len(self.tokenizer.encode("".join(text_list), add_special_tokens=False))
-            text_list.extend([self.PAD_TOKEN] * (len(text_list) - actual_tok_len))
+            if actual_tok_len < len(text_list):
+                text_list.extend([self.PAD_TOKEN] * (len(text_list) - actual_tok_len))
+                return text_list
+
+            # Special case 1-2
+            # 也有case是 ["EP", "EP"]，"EP"會被tokenize成一個token，但"EPEP"會是三個
+            # 在後面補 "DEL_PAD"，代表刪掉後方的一個PAD
+            orig_text_len = sum([len(self.tokenizer.encode(t, add_special_tokens=False)) for t in text_list])  # Encode one token at a time
+            if actual_tok_len > orig_text_len:
+                text_list.extend(["DEL_PAD"] * (actual_tok_len - orig_text_len))
             return text_list
         idx = 0
         buffer = []
@@ -58,6 +84,34 @@ class TextAligner:
             idx += 1
         if buffer:
             new_text.extend(handler(buffer))
+        idx = 0
+        while idx < len(new_text):
+            if new_text[idx] == "DEL_PAD":
+                found_pad = False
+                del new_text[idx]
+                # Find the nex PAD_TOKEN
+                j = 0
+                while idx+j < len(new_text):
+                    if new_text[idx+j] == self.PAD_TOKEN:
+                        del new_text[idx+j]
+                        found_pad = True
+                        break
+                    j += 1
+                # Cannot find the next PAD_TOKEN
+                if not found_pad:
+                    j = -1
+                    while idx+j >= 0:
+                        if new_text[idx+j] == self.PAD_TOKEN:
+                            del new_text[idx+j]
+                            found_pad = True
+                            break
+                        j -= 1
+
+                # There's no PAD_TOKEN, which should not happen
+                if not found_pad:
+                    return None, None
+            idx += 1
+
         text = new_text
         # End special case
 
