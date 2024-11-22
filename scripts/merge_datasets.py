2a5d651355faa6463056975651aa611afddd253f
Anthony Wang <anthony.wang@mygram.ai>
2024-11-22 07:33:58 +0000

    feat: add progress bar

--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -2,9 +2,10 @@ import huggingface_hub
 huggingface_hub.login(token="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")
 from datasets import load_from_disk, concatenate_datasets
 import os
+from tqdm import tqdm
 
 whole_dataset = None
-for ds_dir in os.listdir("./moshi_tts_dataset"):
+for ds_dir in tqdm(os.listdir("./moshi_tts_dataset")):
     try:
         ds = load_from_disk(os.path.join("./moshi_tts_dataset/", ds_dir))
 
