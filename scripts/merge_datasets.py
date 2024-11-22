b225df73637c884d2ab3360a9fe1fc4e66b394ad
Anthony Wang <anthony.wang@mygram.ai>
2024-11-22 07:29:30 +0000

    feat: add merge_datasets.py

---
+++ scripts/merge_datasets.py
@@ -0,0 +1,21 @@
+import huggingface_hub
+huggingface_hub.login(token="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")
+from datasets import load_from_disk, concatenate_datasets
+import os
+
+whole_dataset = None
+for ds_dir in os.listdir("./moshi_tts_dataset"):
+    try:
+        ds = load_from_disk(os.path.join("./moshi_tts_dataset/", ds_dir))
+
+        if not whole_dataset:
+            whole_dataset = ds
+        else:
+            whole_dataset = concatenate_datasets([whole_dataset, ds])
+    except Exception as e:
+        print(str(e))
+        continue
+    
+print(whole_dataset)
+whole_dataset.push_to_hub("anthony-wss/Soundon-tts")
+
