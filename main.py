1780878138b627bb0dd9f256da393311e1ef3bcf
Anthony Wang <anthony.wang@mygram.ai>
2024-11-23 22:59:55 +0000

    feat: support libritts

--- main.py
+++ main.py
@@ -3,41 +3,51 @@ import os
 from multiprocessing import Process, Queue, set_start_method
 import queue
 from module.worker import PreprocessWorker
+from module.datasets.libritts import LibriTTSLoader
 import time
 import torch
+from datasets import Dataset
 
-def worker_func(audio_root_dir, text_root_dir, worker_id, channel_id_queue):
+def worker_func(split_name, worker_id, audio_queue):
     torch.cuda.set_device(worker_id)
-    worker = PreprocessWorker(audio_root_dir, text_root_dir)
+    worker = PreprocessWorker()
+    dataset_dict = {"unit": [], "x-vector": [], "text": [], "text_with_pad": []}
     while True:
         try:
-            channel_id = channel_id_queue.get_nowait()
-            worker.run_channel(channel_id)
+            sample = audio_queue.get_nowait()
+            sample_dict = worker.run_sample(sample)
+            if sample_dict:
+                for key in ["unit", "x-vector", "text", "text_with_pad"]:
+                    dataset_dict[key].extend(sample_dict[key])
         except queue.Empty:
             break
         else:
             time.sleep(.5)
+    ds = Dataset.from_dict(dataset_dict) 
+    ds.save_to_disk(os.path.join("libritts_dataset", split_name, str(worker_id)))
 
 if __name__ == "__main__":
     # Set multiprocessing start method to 'spawn'
     set_start_method("spawn", force=True)
 
     parser = ArgumentParser()
-    parser.add_argument("--audio_root_dir", required=True)
-    parser.add_argument("--text_root_dir", required=True)
+
+    # TODO: Make Soundon works by defining `datasets/soundon.py`
+    parser.add_argument("--audio_root_dir")
+    parser.add_argument("--text_root_dir")
     parser.add_argument("--num_worker", type=int, default=1)
     args = parser.parse_args()
 
-    channel_id_queue = Queue()
-    for channel_id in os.listdir(args.audio_root_dir):
-        if os.path.exists(os.path.join("./moshi_tts_dataset/", channel_id)):
-            continue
-        channel_id_queue.put(channel_id)
+    split_name="dev-clean"
+    dataset = LibriTTSLoader(root_dir="/home/anthony/disk-1t/libritts_pure_audio", split_name=split_name)
+    audio_queue = Queue()
+    for sample in dataset.iterate():
+        audio_queue.put(sample)
 
     number_of_processes = args.num_worker
     processes = []
     for w in range(number_of_processes):
-        p = Process(target=worker_func, args=(args.audio_root_dir, args.text_root_dir, w, channel_id_queue))
+        p = Process(target=worker_func, args=(split_name, w, audio_queue))
         processes.append(p)
         p.start()
 
